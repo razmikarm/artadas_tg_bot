45d36a2c188b048b403d204eb8ef8b12a8f81409
Razmik Poghosyan <razmikarm@gmail.com>
2025-02-24 03:08:44 +0400

    Removed proxy for tma

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from aiogram import Dispatcher, Bot, types
 from app.config import settings
 from app.handlers import router
 from app.middleware import LoggingMiddleware, JWTMiddleware
-from services.tma import router as tma_router
 
 
 log = logging.getLogger("uvicorn")
@@ -45,4 +44,3 @@ async def telegram_webhook(update: dict):
 
 dp.message.middleware(JWTMiddleware())
 app.add_middleware(LoggingMiddleware, debug=settings.DEBUG)
-app.include_router(tma_router)
